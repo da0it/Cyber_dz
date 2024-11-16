99468b0ccb1114a5e1a94fb449d088dca0602730
da0it <nikulas.nikulin@yandex.ru>
2024-11-16 19:41:15 +0300

    Added simple template for site

---
+++ Models/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cyber_dz.Models
+{
+    public class EncryptionModel
+    {
+        public string Input { get; set; }
+        public string EncryptedString { get; set; }
+    }
+}
